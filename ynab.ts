6b2c2728c095f0c2e7a972a57c3cab75636737b4
Benjamin Pannell <benjamin@pannell.dev>
2022-10-01 08:45:48 +0100

    tweak: Improvements to the stock update logic

--- ynab.ts
+++ ynab.ts
@@ -11,11 +11,6 @@ export async function updateAccount(budgetId: string, accountId: string, payeeNa
     const accounts = await api.accounts.getAccountById(budgetId, accountId)
     const account = accounts.data.account
 
-    const payees = await api.payees.getPayees(budgetId)
-    let payee = payees.data.payees.find(p => p.name === payeeName)
-
-    if (!payee) throw new Error(`Payee ${payeeName} not found in budget ${budgetId}`)
-
     const stocks = parseStocksNote(account.note)
 
     const values = await getStockInformation(stocks, budget.data.budget.currency_format.iso_code);
@@ -23,18 +18,17 @@ export async function updateAccount(budgetId: string, accountId: string, payeeNa
     if (!values.length) return;
 
     const shift = Math.floor(values.reduce((sum, stock) => sum + stock.value, 0)*1000) - account.balance;
-    if (!shift) return;
+    if (shift === 0) return;
 
     await api.transactions.createTransaction(budgetId, {
         transaction: {
             account_id: account.id,
             date: new Date().toISOString().split('T')[0],
             amount: shift,
-            payee_name: "Stock Adjustment",
-            cleared: ynab.SaveTransaction.ClearedEnum.Reconciled,
+            payee_name: payeeName,
+            cleared: ynab.SaveTransaction.ClearedEnum.Cleared,
             approved: true,
-            memo: values.map(v => `${v.symbol} ${v.nativeValue}`).join(', '),
-            payee_id: payee.id
+            memo: values.map(v => `${v.symbol} ${v.nativeValue}`).join(', ')
         }
     })
 }
