07845901e6b6a3cd127941c99c9bb84b1bdabb2f
Benjamin Pannell <benjamin@pannell.dev>
2023-01-25 21:35:25 +0000

    tweak: Improve logging of execution progress

--- index.ts
+++ index.ts
@@ -27,17 +27,22 @@ async function updateAccounts(budgetId: string) {
 
         await Promise.all(triggers.map(async trigger => {
             const automation = automations[trigger.kind]
-            if (!automation) return;
+            if (!automation) {
+                console.error(`Unknown automation kind '${trigger.kind}' in account '${account.name}'`)
+                return;
+            }
 
+            console.log(`Running automation '${automation.kind}' in account '${account.name}'`)
             await automation.run(budget.data.budget, account, trigger.options)
+            console.log(`Finished running automation '${automation.kind}' in account '${account.name}'`)
         }))
     }))
 }
 
 
 updateAccounts(budgetId).then(() => {
-    console.log("Updated YNAB account with the latest stock information.")
+    console.log("Updated YNAB account.")
 }).catch(err => {
-    console.error("Failed to update YNAB account with the latest stock information.", err)
+    console.error("Failed to update YNAB account.", err)
     process.exit(1)
 })
